f9f8fc5e565b143f7f2ddb0f8c5b6f630db6269f
jaime <jaime@MacBook-Pro.local>
2021-03-07 11:26:21 -0700

    able to fetch marvel characters

--- assets/dispatch.js
+++ assets/dispatch.js
@@ -1,7 +1,7 @@
 
 // "https://gateway.marvel.com:/v1/public/characters?name=spider-man&ts=1&apikey=cc25a8d815b08a07d1f66b16e2079cff&hash=9e25f1c95eb74bd5d42f42216b82ded3";
 
-let heroName = ["Spider-Man", "thor", 'capitan-america', 'Black Panther', 'dr. strange', 'Antman', 'Iron man', 'Hulk', 'Deadpool', 'GhostRider', 'CaptainMarvel', 'Daredevil'];
+let heroName = ["Spider-Man", "thor", 'captain america', 'Black Panther', 'doctor strange', 'Ant-Man (Scott Lang)', 'Iron man', 'Hulk', 'Deadpool', 'Captain Marvel (Carol Danvers)', 'wolverine', 'Daredevil'];
 
 
 const marvel = {
@@ -48,28 +48,28 @@ heroName.forEach(function (item) {
 
 console.log(marvel + "CODE");
 
-// function displayCharacterOptions() {
-//   fetch(urlApi)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((data) => {
-//       let charNum = 20; //! THIS CAN BE THE NUMBER FOUND IN THE API
-//       for (let i = 0; i < charNum; ++i) {
-//         // let charName = data.data.results[i].name; //! THIS IS THE CHARACTER NAME FROM API
-//         let newChar = document.createElement("label");
-//         newChar.setAttribute("for", "cardoption");
-//         let inputStuff = document.createElement("input");
-//         inputStuff.setAttribute("type", "checkbox");
+function displayCharacterOptions() {
+  fetch(urlApi)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let charNum = 20; //! THIS CAN BE THE NUMBER FOUND IN THE API
+      for (let i = 0; i < charNum; ++i) {
+        // let charName = data.data.results[i].name; //! THIS IS THE CHARACTER NAME FROM API
+        let newChar = document.createElement("label");
+        newChar.setAttribute("for", "cardoption");
+        let inputStuff = document.createElement("input");
+        inputStuff.setAttribute("type", "checkbox");
 
-//         newChar.innerHTML = charName;
-//         newChar.appendChild(inputStuff);
-//         document.querySelector(".scrollmenu").appendChild(newChar);
-//       }
-//     });
-// }
+        newChar.innerHTML = charName;
+        newChar.appendChild(inputStuff);
+        document.querySelector(".scrollmenu").appendChild(newChar);
+      }
+    });
+}
 
-// displayCharacterOptions();
+displayCharacterOptions();
 
 const randomMovie = {};
 
