230c3a9123f42772bfe1982266de2b041eeda21d
jaime <jaime@MacBook-Pro.local>
2021-03-10 23:52:24 -0700

    trying to get movie.html to work

--- assets/dispatch.js
+++ assets/dispatch.js
@@ -1,6 +1,3 @@
-
-// "https://gateway.marvel.com:/v1/public/characters?name=spider-man&ts=1&apikey=cc25a8d815b08a07d1f66b16e2079cff&hash=9e25f1c95eb74bd5d42f42216b82ded3";
-// mavel characters list
 let heroName = ["Spider-Man", "thor", 'captain america', 'Black Panther', 'doctor strange', 'Ant-Man (Scott Lang)', 'Iron man', 'Hulk', 'Deadpool', 'wolverine', 'Captain Marvel (Carol Danvers)', 'Daredevil'];
 
 // fucntion to fetch marvel charactes img, name, bio url and comics url
@@ -18,7 +15,6 @@ const marvel = {
         for (var i = 0; i < json.data.results.length; i++) {
           let name = json.data.results[i].name
           let heroImg = `<img class="logo" src="${json.data.results[i].thumbnail.path}.${json.data.results[i].thumbnail.extension}" alt="${name}"style="width:296px;height:396px;">`
-          // let herobio = json.data.results[i].description
           let heroBio = json.data.results[i].urls[1].url
           let heroComic = json.data.results[i].urls[0].url
 
@@ -40,12 +36,10 @@ const marvel = {
                           <button class="btn-${i + 1} button content is-small level-item is-warning is-rounded"
                               onClick="javascript:window.open('${heroComic}', '_blank');">${name}'s
                               Comics</button>
-                          <button class="button content is-small is-warning level-item  is-rounded"
-                              onClick="javascript:window.open('./movie.html', '_blank');">Movies</button>
+                          <button class="btn-movie button content is-small is-warning level-item  is-rounded" value="${name}">Movies</button>
                       </div>
                   </div>
               </div>
-
          
 `)
         }
@@ -59,28 +53,47 @@ heroName.forEach(function (item) {
 
 })
 
+$(document).ready(function () {
+  $(".btn-movie").click(function (event) {
+    // event.preventDefault();
+    // if (movieTitle === null) {
+    //   console.log("Element is undefined");
 
-
-console.log(marvel + "CODE");
-
-
-function displayCharacterOptions() {
-
-  fetch(urlApi)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      let charNum = 20; //! THIS CAN BE THE NUMBER FOUND IN THE API
-      for (let i = 0; i < charNum; ++i) {
-        // let charName = data.data.results[i].name; //! THIS IS THE CHARACTER NAME FROM API
-        let newChar = document.createElement("label");
-        newChar.setAttribute("for", "cardoption");
-        let inputStuff = document.createElement("input");
-        inputStuff.setAttribute("type", "checkbox");
-      };
+    // } else {
+    getMovie(movieTitle);
   });
-          };
+});
 
+var movieTitle = $('.btn-movie').val();
+
+function getMovie(movieTitle) {
+  fetch(
+    "http://www.omdbapi.com/?s=" +
+    movieTitle +
+    // "&y=" +
+    // year +
+    "&apikey=8f0e2144"
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+
+      // var link = json.Search[0].Poster;
+      const array = json.Search.slice(0, 10)
+      // console.log(array)
+      for (let i = 0; i < array.length; i++) {
+        const movie = array[i];
+        console.log(movie)
+
+        const img = document.createElement("img")
+        // console.log(array)
+        // console.log(movie.Poster)
+        img.setAttribute("src", movie.Poster)
+        document.querySelector("#img").appendChild(img)
+      }
+      // build a 4 loop  and  then array slice method.
+
+    });
+}
 
 
